e656cda3690233a2dd9c5bc53027c1539ced070b
Airat Murtazin <ayrat.murtazin@mail.ru>
2023-10-24 21:10:11 +0300

    make starting version of a  countdown timer

--- challenge-#1/app.js
+++ challenge-#1/app.js
@@ -1,1 +1,29 @@
 'use strict'
+
+const startingMinutes = 15
+let time = startingMinutes * 60
+let intervalId // Variable to store the interval ID
+let mins = document.querySelector('.minutes')
+let secs = document.querySelector('.seconds')
+const startBtn = document.querySelector('.start')
+
+startBtn.addEventListener('click', () => {
+	clearInterval(intervalId)
+
+	intervalId = setInterval(updateCountDown, 1000)
+})
+
+function updateCountDown() {
+	let minutes = Math.floor(time / 60)
+	let seconds = time % 60
+
+	seconds = seconds < 10 ? '0' + seconds : seconds
+
+	mins.innerHTML = `<input type="text" value="${minutes}" />`
+	secs.innerHTML = `<input type="text" value="${seconds}" />`
+	time--
+
+	if (time < 0) {
+		clearInterval(intervalId)
+	}
+}
